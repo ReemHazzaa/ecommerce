446161af222e784e12292d4cd3a3b3a4fc206cca
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-08-09 15:31:02 +0200

    add getAllProducts to FakeStoreApiService

--- app/src/main/java/com/extreamSolution/ecommerce/data/remote/apiService/FakeStoreApiService.kt
+++ app/src/main/java/com/extreamSolution/ecommerce/data/remote/apiService/FakeStoreApiService.kt
@@ -1,9 +1,13 @@
 package com.extreamSolution.ecommerce.data.remote.apiService
 
+import com.extreamSolution.ecommerce.domain.models.product.Product
 import retrofit2.http.GET
 
 interface FakeStoreApiService {
 
     @GET("products/categories")
     suspend fun getCategoriesList(): List<String>
+
+    @GET("products")
+    suspend fun getAllProducts(): List<Product>
 }
---
+++ app/src/main/java/com/extreamSolution/ecommerce/domain/models/product/Rating.kt
@@ -0,0 +1,6 @@
+package com.extreamSolution.ecommerce.domain.models.product
+
+data class Rating(
+    val count: Int,
+    val rate: Double
+)
