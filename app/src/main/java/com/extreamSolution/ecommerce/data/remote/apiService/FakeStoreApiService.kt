7e0f06fac47859e62f6afef17d9a87de8fe79287
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-08-09 15:27:10 +0200

    add getCategoriesList to FakeStoreApiService

--- app/src/main/java/com/extreamSolution/ecommerce/data/remote/apiService/FakeStoreApiService.kt
+++ app/src/main/java/com/extreamSolution/ecommerce/data/remote/apiService/FakeStoreApiService.kt
@@ -1,5 +1,9 @@
 package com.extreamSolution.ecommerce.data.remote.apiService
 
+import retrofit2.http.GET
+
 interface FakeStoreApiService {
 
+    @GET("products/categories")
+    suspend fun getCategoriesList(): List<String>
 }
