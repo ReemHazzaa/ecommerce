f4c75b81659c9ebe3bd0eb537a8d85e84027c877
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-08-09 17:22:31 +0200

    Create GetProductsUseCase.kt

---
+++ app/src/main/java/com/extremeSolution/ecommerce/domain/usecases/products/GetProductsUseCase.kt
@@ -0,0 +1,15 @@
+package com.extremeSolution.ecommerce.domain.usecases.products
+
+import com.extremeSolution.ecommerce.domain.models.product.Product
+import com.extremeSolution.ecommerce.domain.repo.AppRepo
+import com.extremeSolution.ecommerce.domain.usecases.baseUseCase.BaseUseCase
+import retrofit2.Response
+import javax.inject.Inject
+
+class GetProductsUseCase @Inject constructor(private val appRepo: AppRepo) :
+    BaseUseCase<Any, Response<List<Product>>> {
+    override suspend fun execute(params: Any?): Response<List<Product>> {
+        return appRepo.getAllProducts()
+    }
+
+}
