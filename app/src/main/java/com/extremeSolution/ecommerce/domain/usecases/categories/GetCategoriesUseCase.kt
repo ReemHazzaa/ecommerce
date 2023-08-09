40a55bfba6dbc797af1cbbc67ffbc077e82ce497
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-08-09 17:21:27 +0200

    Create GetCategoriesUseCase.kt

---
+++ app/src/main/java/com/extremeSolution/ecommerce/domain/usecases/categories/GetCategoriesUseCase.kt
@@ -0,0 +1,14 @@
+package com.extremeSolution.ecommerce.domain.usecases.categories
+
+import com.extremeSolution.ecommerce.domain.repo.AppRepo
+import com.extremeSolution.ecommerce.domain.usecases.baseUseCase.BaseUseCase
+import retrofit2.Response
+import javax.inject.Inject
+
+class GetCategoriesUseCase @Inject constructor(private val appRepo: AppRepo) :
+    BaseUseCase<Any, Response<List<String>>> {
+    override suspend fun execute(params: Any?): Response<List<String>> {
+        return appRepo.getCategoriesList()
+    }
+
+}
