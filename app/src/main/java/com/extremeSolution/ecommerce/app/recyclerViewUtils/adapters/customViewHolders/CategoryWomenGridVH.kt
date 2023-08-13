bd712a8badf1cf341a33d3d583da96bcc084e31a
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-08-13 11:24:57 +0200

    remove data binding from products in home

--- app/src/main/java/com/extremeSolution/ecommerce/app/recyclerViewUtils/adapters/customViewHolders/CategoryWomenGridVH.kt
+++ app/src/main/java/com/extremeSolution/ecommerce/app/recyclerViewUtils/adapters/customViewHolders/CategoryWomenGridVH.kt
@@ -3,6 +3,8 @@ package com.extremeSolution.ecommerce.app.recyclerViewUtils.adapters.customViewH
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.extremeSolution.ecommerce.app.extensions.loadImageWithPicasso
+import com.extremeSolution.ecommerce.app.extensions.setDoubleValueToTextView
 import com.extremeSolution.ecommerce.databinding.ItemCategoryWomenBinding
 import com.extremeSolution.ecommerce.domain.models.product.Product
 
@@ -10,8 +12,11 @@ class CategoryWomenGridVH(private val binding: ItemCategoryWomenBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
     fun bind(product: Product) {
-        binding.item = product
-        binding.executePendingBindings()
+        binding.apply {
+            tvPrice.setDoubleValueToTextView(product.price)
+            tvTitle.text = product.title
+            ivImage.loadImageWithPicasso(product.image)
+        }
     }
 
     companion object {
